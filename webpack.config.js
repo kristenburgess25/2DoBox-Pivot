637eb7348673dbadd91ebbe8bd95862640eedc87
lrknaff <lacey.knaff@gmail.com>
2016-10-04 13:20:25 -0600

    added babel and sass dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const path = require('path');
+
 module.exports = {
   entry: {
     main: "./lib/index.js",
@@ -8,5 +10,15 @@ module.exports = {
   output: {
     path: __dirname,
     filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loader: "style!css!sass" }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.css']
   }
 }
