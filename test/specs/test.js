8b9da8b46f21f08cb85fc7b5cf60cda12f4a02f4
Kristen Burgess <kristen.burgess@doane.edu>
2016-10-06 16:02:42 -0600

    dom testing be lit

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,1 +1,16 @@
-const assert = require('assert')
+var assert = require('assert')
+describe('this is a warm up test', function(){
+  it('should have an input field and I should be able to set a value inside of it', function(){
+    // i need to define a browser (this is what i'll use to grab my shit)
+    browser.url('/');
+
+
+    // browser will let me find a specific element
+    var titleField = browser.element('#title-input');
+    // when I have the specific I will set a value inside of that element
+    titleField.setValue('some great title');
+    // I will then check the field to see if that element is there. // that is my assert
+
+    assert.equal(titleField.getValue(), 'some great title');
+  });
+});
