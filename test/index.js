0094a332b67335008909c478514dbbf4e6631f62
Kristen Burgess <kristen.burgess@doane.edu>
2016-10-09 18:07:41 -0600

    mock local storage intalled, unit testing still sucks

--- test/index.js
+++ test/index.js
@@ -6,19 +6,3 @@ require('./unitTests.js');
 //     assert(true);
 //   });
 //   });
-
-  // describe('Task Object', function(){
-  // var task = new Task();
-  //
-  // it('Task should be an object', function(){
-  //   assert.isObject(task, true);
-  // });
-  // //
-  // // it.skip('Task should have a default importance of normal/votes[2]', function(){
-  // //   assert.equal(task.importance, votes[2]);
-  // // });
-  // //
-  // // it.skip('Task should have a default status status of incomplete', function(){
-  // //   assert.equal(task.status, 'incomplete');
-  // // });
-  //   });
--- test/unitTests.js
+++ test/unitTests.js
@@ -7,18 +7,21 @@ describe('our test bundle', function () {
   });
 });
 
-describe('Task Object', function(){
-var task = new Task();
+describe('Task', function(){
+// it('should be a function', function() {
+//   assert.isFunction(Task);
+// });
 
-it('Task should be an object', function(){
-  assert.isObject(task, true);
+it('should instantiate a task object', function(){
+  // var task = new Task();
+  assert.isObject(Task);
 });
 //
-// it.skip('Task should have a default importance of normal/votes[2]', function(){
-//   assert.equal(task.importance, votes[2]);
+// it('Task should have a default importance of normal/votes[2]', function(){
+//   assert.equal(this.importance, votes[2]);
 // });
 //
-// it.skip('Task should have a default status status of incomplete', function(){
-//   assert.equal(task.status, 'incomplete');
+// it('Task should have a default status status of incomplete', function(){
+//   assert.equal(this.status, 'incomplete');
 // });
   });
